7f65e005381fa0aacbef7adecef77e7607b9f4dd
SamuelSgn <samuel.soglohoun@epitech.eu>
2024-09-13 23:27:22 +0100

    changing stack modules and displaying port value

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
+require('dotenv').config()
 const mysql = require('mysql2');
 const db = require('./config/db');
-const express = require('express');
+const fs = require('fs');
+const express = require("express");
 const app = express();
-const {checkEmail, checkPassword} = require('./middleware/auth');
-const have = require('dotenv');
-have.config();
-let port = process.env.PORTES;
+const {checkEmail, checkPassword, authToken} = require('./middleware/auth');
+const port = process.env.PORT;
+const cors = require('cors');
 const body = require('body-parser');
 const Roads = require('./routes/auth/auth');
 
-app.get('/', (req, res) => {
+app.post('/info', (req, res) => {
     res.send("Hello World!");
 });
+
+app.use(cors());
 
 app.use(body.urlencoded({extended: false}));
 app.use(body.json());
 
 app.use(Roads);
 
-app.listen(3500, () =>{
-    console.log('Example app listening at http://localhost:${3500}');
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:' + port);
 });

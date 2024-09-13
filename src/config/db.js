af373e113a27dd9dd731eed39b3968da0cdfd89c
SamuelSgn <samuel.soglohoun@epitech.eu>
2024-09-13 23:28:50 +0100

    adding space

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if(err){
+    if (err) {
         console.error('Cannot be connected' + err.message);
         return;
     }
